18186c99f274a4eac5a07a42b0f8390bb2af546b
Mohamed Abdul <mohamedyousufmo@gmail.com>
2022-02-22 12:21:48 -0800

    added java collection list

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   LIST_TITLE_URL : "../assets/data/list-data.json",
-  ARRAY_URL : "../assets/data/array.json"
+  ARRAY_URL : "../assets/data/array.json",
+  JAVA_COLLECTION_LIST : "../assets/data/java_collections.json"
 };
